50d5058e6d3a9ea9b0d524eef9158b53426815d3
suneelkumar-24 <k163997@nu.edu.pk>
2023-06-29 00:18:44 +0500

    d

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json())
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://neuappliances.vercel.app',
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
